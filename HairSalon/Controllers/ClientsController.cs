588dfec4adbc15192705c6278361922920c9b48a
Nick Tse <nicholashtse@gmail.com>
2022-08-03 18:48:16 -0700

    update ClientsController to include mvc rendering directive

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using HairSalon.Models;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace HairSalon.Controllers
 {
